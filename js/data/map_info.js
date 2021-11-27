41e7c2302f983ddc5a6af1720479f20addbf5ebd
iwabuchi-yuki-butchi <butc.hiyu+com-sourcetreeapp@gmail.com>
2021-11-27 21:25:40 +0900

    マップ情報追加

--- js/data/map_info.js
+++ js/data/map_info.js
@@ -1,8 +1,108 @@
 var spotLi = {
-  "1": {
-    title: 'タイトル',
-    title_en: 'Title',
-    lat: 71.91088787611528,
-    lng: 123.57421875,
-  },
-};
+  "sheet": {
+    "1": {
+      "title": "稲妻",
+      "x": 3570,
+      "y": 2598,
+      "re": 0.441544766781024,
+      "im": -0.376631785038623,
+      "youtube-url": "https://youtu.be/Qf1Q_HgTWdM"
+    },
+    "2": {
+      "title": "イソギンチャクの群体",
+      "x": 1974,
+      "y": 1349,
+      "re": -0.649797704293039,
+      "im": 0.477084285661745,
+      "youtube-url": "https://youtu.be/j03p-irxLt4"
+    },
+    "3": {
+      "title": "1兆x1兆倍の彼方",
+      "x": 1079,
+      "y": 2114,
+      "re": -1.26222162762384,
+      "im": -0.0459170016351388,
+      "youtube-url": "https://youtu.be/gva-evKs_8A"
+    },
+    "4": {
+      "title": "龍の巣",
+      "x": 1567,
+      "y": 2400,
+      "re": -0.928145299908501,
+      "im": -0.241136170655592,
+      "youtube-url": "https://youtu.be/fNJ1JWY_GaU"
+    },
+    "5": {
+      "title": "海藻",
+      "x": 3471,
+      "y": 2919,
+      "re": 0.373888457620489,
+      "im": -0.596176168021392,
+      "youtube-url": "https://youtu.be/zltFe4bz2fw"
+    },
+    "6": {
+      "title": "火の粉",
+      "x": 3269,
+      "y": 2811,
+      "re": 0.235458712159898,
+      "im": -0.52252323018718,
+      "youtube-url": "https://youtu.be/VICWRxanLKM"
+    },
+    "7": {
+      "title": "ニードル",
+      "x": 2883,
+      "y": 594,
+      "re": -0.0282250251091347,
+      "im": 0.993845785431388,
+      "youtube-url": "https://youtu.be/-bj4oMeNMkY"
+    },
+    "8": {
+      "title": "レインボーサイクロン",
+      "x": 3452,
+      "y": 1523,
+      "re": 0.360499920380204,
+      "im": 0.358272305109354,
+      "youtube-url": "https://youtu.be/BPd7Ud4CnP4"
+    },
+    "9": {
+      "title": "パセリ",
+      "x": 87,
+      "y": 2047,
+      "re": -1.9399135918605,
+      "im": 0,
+      "youtube-url": "https://youtu.be/3kDde-t6w0I"
+    },
+    "10": {
+      "title": "霜雪",
+      "x": 1083,
+      "y": 1487,
+      "re": -1.25902049465916,
+      "im": 0.383134962304845,
+      "youtube-url": "https://youtu.be/vjyJatMiBbs"
+    },
+    "11": {
+      "title": "バンザイ",
+      "x": 2383,
+      "y": 2991,
+      "re": -0.370434491138992,
+      "im": -0.645528635289903,
+      "youtube-url": "https://youtu.be/klvHhFakOkc"
+    },
+    "12": {
+      "title": "宇宙の泡構造",
+      "x": 1826,
+      "y": 2010,
+      "re": -0.750810235588225,
+      "im": 0.0252227665713417,
+      "youtube-url": "https://youtu.be/91Gi7IslKQE"
+    },
+    "13": {
+      "title": "峡谷に潜む蝶",
+      "x": 2698,
+      "y": 3555,
+      "re": -0.154687419237845,
+      "im": -1.03102447707107,
+      "youtube-url": "https://youtu.be/NrnvfqqDWW8"
+    }
+  }
+}
